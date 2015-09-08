4b538002bf50ebe61ceaced9ee1ff9afce34d5de
Jorge Vila <jorge.vila@gmail.com>
2015-09-08 18:08:59 +0200

    View example: example for UIApplication creation

---
+++ ViewExample/ViewExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViewExample
+//
+//  Created by jorgev on 8/9/15.
+//  Copyright (c) 2015 jorgev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewExample/ViewExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ViewExample
+//
+//  Created by jorgev on 8/9/15.
+//  Copyright (c) 2015 jorgev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
